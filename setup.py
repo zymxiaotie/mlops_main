fde5a514c60a1be55bd392957e6521c88e237a99
zymxiaotie <zymxiaotie@gmail.com>
2023-11-07 10:12:43 +0800

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='exercise project with github using cookiecutter frame',
+    author='zymxiaotie',
+    license='MIT',
+)
