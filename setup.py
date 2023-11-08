5d5a78533bcb03a8214a2749bf4c49437b7d2b31
May <zymxiaotie@gmail.com>
2023-11-08 10:26:30 +0800

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='repeat mlops_main to solve app issue',
+    author='zymxiaotie',
+    license='MIT',
+)
